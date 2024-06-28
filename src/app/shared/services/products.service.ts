04232cdc9181e0b6760738537597636fa404d24f
Eliomar Campos <eliomar.systems@gmail.com>
2024-06-28 09:39:30 -0300

    feat: adiciona funcionalidade de salvar produto

--- src/app/shared/services/products.service.ts
+++ src/app/shared/services/products.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Product } from '../interfaces/product.interface';
+import { ProductPayload } from '../interfaces/payload-product.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,10 @@ export class ProductsService {
 
   getAll(){
     return this.httpClient.get<Product[]>('api/products');
+  }
+
+  post(payload: ProductPayload){
+    return this.httpClient.post('api/products', payload);
   }
 
 
